289629379928402572d153bc2ca6bb8d3de4a980
Mikelarg <murtazkorpaz@gmail.com>
2016-01-29 23:57:26 +0800

    Added events + minor fixes + added author

--- checkform.js
+++ checkform.js
@@ -1,3 +1,4 @@
+/* Created by Mikelarg (c) 2015 */
 var checkValI= 0;//variable for id of message for input
 (function ( $ ) {
 	$.fn.checkForm = function(options){
@@ -6,12 +7,11 @@ var checkValI= 0;//variable for id of message for input
 		 Variables
 		 */
 
-		var nec_inp = this.find('input[inputnec]').length; //number of necessarily inputs
-		var submitbutton = this.find('input[submitcheck]'); //submitbutton
+		var necessaryInput = this.find('input[inputnec]').length; //number of necessarily inputs
+		var submitButton = this.find('input[submitcheck]'); //submit button
 		var form = this; //form
 		var inputCheck = this.find('input[inputcheck]');
 		var inputCheckAjax = this.find('input[inputcheckajax]');
-		var blurInp =  this.find('input[type=text]');
 
 		options = filterOptions(options);
 
@@ -31,9 +31,11 @@ var checkValI= 0;//variable for id of message for input
 		 Some events
 		 */
 
-		form.attr('nec_inp',nec_inp);
-		if(nec_inp>0)
-			submitbutton.prop('disabled',true);
+		form.attr('nec_inp',necessaryInput);
+		if(necessaryInput>0)
+			submitButton.prop('disabled',true);
+
+		inputCheck.add(inputCheckAjax).unbind("blur", onBlur);
 		inputCheck.add(inputCheckAjax).blur(onBlur);
 	};
 
@@ -43,9 +45,8 @@ var checkValI= 0;//variable for id of message for input
 		 Variables
 		 */
 		var form = this;//form
-		var nec_inp = this.find('input[inputnec]').length; //number of necessarily inputs
-		var submitbutton = this.find('input[submitcheck]'); //submitbutton
-		var blurInp =  this.find('input[type=text]');
+		var necessaryInput = this.find('input[inputnec]').length; //number of necessarily inputs
+		var submitButton = this.find('input[submitcheck]'); //submit button
 
 		options = filterOptions(options);
 
@@ -64,27 +65,29 @@ var checkValI= 0;//variable for id of message for input
 		 Some events
 		 */
 
-		form.attr('nec_inp', nec_inp);
-		if(nec_inp > 0)
-			submitbutton.prop('disabled', true);
+		form.attr('nec_inp', necessaryInput);
+		if(necessaryInput > 0)
+			submitButton.prop('disabled', true);
+
+		elem.unbind('blur', onBlur);
 		elem.blur(onBlur);
 	};
 
 	$.fn.checkInput = function() { //blurInput- that function checks your input(event - can be jQuery object)
 		var elem = this;
-		var neces = !CheckFormHelper.isUndefined(elem.attr('inputnec'));
-		var calfunc = !CheckFormHelper.isUndefined(elem.attr('inputcheck')) && elem.attr('inputcheck') != '';
-		var calfuncajax = !CheckFormHelper.isUndefined(elem.attr('inputcheckajax'));
+		var necessary = !CheckFormHelper.isUndefined(elem.attr('inputnec'));
+		var callFunc = !CheckFormHelper.isUndefined(elem.attr('inputcheck')) && elem.attr('inputcheck') != '';
+		var calFuncAjax = !CheckFormHelper.isUndefined(elem.attr('inputcheckajax'));
 		var arguments = CheckFormHelper.defaultParameter(elem.attr('inputarg'), '');
 
 		var options = elem.data('options');
 		var addHTML = options.addHTML;
 
 		arguments = JSON.parse('{'+arguments+'}');
-		if(calfunc || calfuncajax) {
+		if(callFunc || calFuncAjax) {
 			if(elem.val()!='') {
 				var func;
-				if(calfuncajax) {
+				if(calFuncAjax) {
 					func = elem.attr('inputcheckajax');
 					window[func](elem,elem.val(), arguments);
 				}
@@ -92,20 +95,42 @@ var checkValI= 0;//variable for id of message for input
 					func = elem.attr('inputcheck');
 					var returnedVal = window[func](elem.val(), arguments);
 					if(returnedVal) {
-						CheckFormHelper.addRight(elem, neces, addHTML);
+						CheckFormHelper.addRight(elem, addHTML);
 					}else {
-						CheckFormHelper.addError(elem, neces, elem.attr('text_error'), addHTML);
+						CheckFormHelper.addError(elem, elem.attr('text_error'), addHTML);
 					}
 				}
 			}else {
-				CheckFormHelper.removeRight(elem, neces);
+				CheckFormHelper.removeRight(elem);
 			}
 		}
 		else {
-			CheckFormHelper.typical(elem, neces, addHTML);
+			CheckFormHelper.typical(elem, addHTML);
 		}
-		if(neces&&!calfuncajax)
+		if(necessary&&!calFuncAjax)
 			CheckFormHelper.formValid(this.parents('form[formcheck]'));
+	};
+
+	$.fn.onRightInput = function(event, override) {
+		override = CheckFormHelper.defaultParameter(override, false);
+		this.data('overrideOnRightInput', override);
+		this.bind('onRightInput', event);
+	};
+
+	$.fn.onWrongInput = function(event, override) {
+		override = CheckFormHelper.defaultParameter(override, false);
+		this.data('overrideOnWrongInput', override);
+		this.bind('onWrongInput', event);
+	};
+
+	$.fn.onRemoveStateInput = function(event, override) {
+		override = CheckFormHelper.defaultParameter(override, false);
+		this.data('overrideOnRemoveStateInput', override);
+		this.bind('onRemoveStateInput', event);
+	};
+
+	$.fn.changeErrorMessage = function(message) {
+		this.attr('text_error', message);
 	};
 
 	function onBlur(event) {
@@ -125,101 +150,107 @@ var checkValI= 0;//variable for id of message for input
 function CheckFormHelper() {}
 
 CheckFormHelper.checkValAjax = function(elem, val, addHTML) {
-	var neces = !CheckFormHelper.isUndefined(jQuery(this).attr('inputnec'));
-
 	addHTML = CheckFormHelper.defaultParameter(addHTML,'<div class = "valCheck"><div class="valCheckSymbol"></div> <div class = "valCheckText"></div></div>');
 	if(val) {
-		CheckFormHelper.addRight(elem, neces, addHTML);
+		CheckFormHelper.addRight(elem, addHTML);
 	}else {
-		CheckFormHelper.addError(elem, neces, elem.attr('text_error'), addHTML);
+		CheckFormHelper.addError(elem, elem.attr('text_error'), addHTML);
 	}
 	CheckFormHelper.formValid(elem.parents('form[formcheck]'));
 };
 
-CheckFormHelper.typical = function(elem,sm,html) {
-	if(elem.val()=='')
-		CheckFormHelper.removeRight(elem,sm,html);
+CheckFormHelper.typical = function(elem, html) {
+	if(elem.val() == '')
+		CheckFormHelper.removeRight(elem, html);
 	else if(elem.val())
-		CheckFormHelper.addRight(elem,sm,html);
+		CheckFormHelper.addRight(elem, html);
 };
 
 //add message to inputs after filling them
-CheckFormHelper.addError = function(elem, sm, text, html) {//function if input filled wrong(elem - (jQuery Object) jQuery element, sm - (bool) input is necessarily?, text - text of error)
-	sm = CheckFormHelper.defaultParameter(sm, false);
+CheckFormHelper.addError = function(elem, text, html) {//function if input filled wrong(elem - (jQuery Object) jQuery element, sm - (bool) input is necessarily?, text - text of error)
+	var override = elem.data('overrideOnWrongInput');
 
-	if(CheckFormHelper.isUndefined(elem.attr('id_input_message'))){
-		elem.after(html);
-		elem.next().attr('valCheckId',checkValI);
+	elem.attr('closed', '-');
 
-		elem.attr('id_input_message',checkValI);
-		checkValI++;
-	}
-
-	var id = elem.attr('id_input_message');
-	var valCheckElem = jQuery('*[valCheckId='+id+']');
-	var valCheckElemText = jQuery('*[valCheckId='+id+'] .valCheckText');
-
-	if(valCheckElem.attr('right') == 1){
-		valCheckElemText.animate({opacity:1},100);
-		elem.removeClass('right');
-	}
-
-	elem.attr('closed','-');
-
+	if (elem.hasClass('right')) elem.removeClass('right');
 	elem.addClass('wrong');
 
-	valCheckElem.attr('right', 0);
+	if (!override) {
+		if (CheckFormHelper.isUndefined(elem.attr('id_input_message'))) {
+			elem.after(html);
+			elem.next().attr('valCheckId', checkValI);
 
-	var elemPosition = elem.offset();
-	var left = elemPosition.left + elem.outerWidth(true) + 5;
-	var top = elemPosition.top + (elem.outerHeight(true) - valCheckElem.outerHeight(true))/2;
-	valCheckElem.offset({top: top, left: left});
+			elem.attr('id_input_message', checkValI);
+			checkValI++;
+		}
 
-	valCheckElemText.text(text);
-	valCheckElem.animate({opacity:1},500);
+		var id = elem.attr('id_input_message');
+		var valCheckElem = jQuery('*[valCheckId=' + id + ']');
+		var valCheckElemText = jQuery('*[valCheckId=' + id + '] .valCheckText');
+
+		if (valCheckElem.attr('right') == 1) valCheckElemText.animate({opacity: 1}, 100);
+
+		valCheckElem.attr('right', 0);
+
+		var elemPosition = elem.offset();
+		var left = elemPosition.left + elem.outerWidth(true) + 5;
+		var top = elemPosition.top + (elem.outerHeight(true) - valCheckElem.outerHeight(true)) / 2;
+		valCheckElem.offset({top: top, left: left});
+
+		valCheckElemText.text(text);
+		valCheckElem.animate({opacity: 1}, 500);
+	}
+
+	elem.trigger('onWrongInput', html, text);
 	CheckFormHelper.formValid(elem.parents('form[formcheck]'));
 };
 
-CheckFormHelper.removeRight = function(elem, sm) {//function if input clean(elem - (jQuery Object) jQuery element, sm - (bool) input is necessarily?)
-	sm = CheckFormHelper.defaultParameter(sm, false);
+CheckFormHelper.removeRight = function(elem) {//function if input clean(elem - (jQuery Object) jQuery element, sm - (bool) input is necessarily?)
 	var id = elem.attr('id_input_message');
-	if(!CheckFormHelper.isUndefined(id)) {
-		jQuery('*[valCheckId='+id+']').animate({opacity:0},100);
-		elem.attr('closed','none');
-		elem.removeClass('right');
-		elem.removeClass('wrong');
-	}
+	var override = elem.data('overrideOnRemoveStateInput');
+
+	elem.attr('closed','none');
+	elem.removeClass('right');
+	elem.removeClass('wrong');
+
+	if (!CheckFormHelper.isUndefined(id) && !override) jQuery('*[valCheckId='+id+']').animate({opacity:0},100);
+
+	elem.trigger('onRemoveStateInput');
 	CheckFormHelper.formValid(elem.parents('form[formcheck]'));
 };
 
-CheckFormHelper.addRight = function(elem, sm, html) {//function if input filled right(elem - (jQuery Object) jQuery element, sm - (bool) input is necessarily?)
-	sm = CheckFormHelper.defaultParameter(sm, false);
-	if (CheckFormHelper.isUndefined(elem.attr('id_input_message'))){
-		elem.after(html);
-		elem.next().attr('valCheckId',checkValI);
+CheckFormHelper.addRight = function(elem, html) {//function if input filled right(elem - (jQuery Object) jQuery element, sm - (bool) input is necessarily?)
+	var override = elem.data('overrideOnRightInput');
 
-		elem.attr('id_input_message',checkValI);
-		checkValI++;
-	}
-	var id = elem.attr('id_input_message');
-	var valCheckElem = jQuery('*[valCheckId='+id+']');
-	var valCheckElemText = jQuery('*[valCheckId='+id+'] .valCheckText');
-
-	if(valCheckElem.attr('right') == 0){
-		valCheckElemText.animate({opacity:0}, 100);
-		elem.removeClass('wrong');
-	}
-
-	valCheckElem.attr('right', 1);
+	if (elem.hasClass('wrong')) elem.removeClass('wrong');
 	elem.addClass('right');
 
-	var elemPosition = elem.offset();
-	var left = elemPosition.left + elem.outerWidth(true) + 5;
-	var top = elemPosition.top + (elem.outerHeight(true) - valCheckElem.outerHeight(true))/2;
-	valCheckElem.offset({top: top, left: left});
-
 	elem.attr('closed', '+');
-	valCheckElem.animate({opacity:1}, 100);
+
+	if (!override) {
+		if (CheckFormHelper.isUndefined(elem.attr('id_input_message'))) {
+			elem.after(html);
+			elem.next().attr('valCheckId', checkValI);
+
+			elem.attr('id_input_message', checkValI);
+			checkValI++;
+		}
+		var id = elem.attr('id_input_message');
+		var valCheckElem = jQuery('*[valCheckId=' + id + ']');
+		var valCheckElemText = jQuery('*[valCheckId=' + id + '] .valCheckText');
+
+		if (valCheckElem.attr('right') == 0) valCheckElemText.animate({opacity: 0}, 100);
+
+		valCheckElem.attr('right', 1);
+
+		var elemPosition = elem.offset();
+		var left = elemPosition.left + elem.outerWidth(true) + 5;
+		var top = elemPosition.top + (elem.outerHeight(true) - valCheckElem.outerHeight(true)) / 2;
+		valCheckElem.offset({top: top, left: left});
+		valCheckElem.animate({opacity:1}, 100);
+	}
+
+	elem.trigger('onRightInput', html);
 	CheckFormHelper.formValid(elem.parents('form[formcheck]'));
 };
 
